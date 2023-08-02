e63e8eef3f46d6cc9375ad82263176d9f158519a
Siddhesh <siddheshgovalkar1967@gmail.com>
2023-08-02 22:03:05 +0530

    added new project

--- script.js
+++ script.js
@@ -80,8 +80,30 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 // Add auto Scroll Functionality for Project Images if they are Uploaded more than 3.
-  function rotateImages() {
-    const screenshotsInner = document.querySelector('.screenshots-inner');
+  // function rotateImages() {
+  //   const screenshotsInner = document.querySelector('.screenshots-inner');
+  //   const screenshotWidth = screenshotsInner.firstElementChild.clientWidth;
+  //   const scrollAmount = screenshotWidth + 10; // Adjust this value to change the scroll distance
+    
+  //   const currentPosition = Math.abs(parseInt(screenshotsInner.style.transform.split('(')[1])) || 0;
+    
+  //   if (currentPosition >= screenshotsInner.scrollWidth - screenshotsInner.clientWidth) {
+  //     screenshotsInner.style.transform = 'translateX(0)';
+  //   } else {
+  //     screenshotsInner.style.transform = `translateX(-${currentPosition + scrollAmount}px)`;
+  //   }
+    
+  //   // Check if the entire images have scrolled out of the container
+  //   if (currentPosition >= screenshotsInner.scrollWidth) {
+  //     screenshotsInner.style.transform = 'translateX(0)';
+  //   }
+  // }
+  
+  // setInterval(rotateImages, 2000);
+
+
+  function rotateImages(containerId) {
+    const screenshotsInner = document.querySelector(`#${containerId} .screenshots-inner`);
     const screenshotWidth = screenshotsInner.firstElementChild.clientWidth;
     const scrollAmount = screenshotWidth + 10; // Adjust this value to change the scroll distance
     
@@ -98,8 +120,9 @@ document.addEventListener('DOMContentLoaded', function() {
       screenshotsInner.style.transform = 'translateX(0)';
     }
   }
-  
-  setInterval(rotateImages, 2000);
+
+  setInterval(() => rotateImages('carrental-project'), 2000);
+  setInterval(() => rotateImages('technomarket-project'), 2000);
 
 
 
